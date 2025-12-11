8b5cfbcd3109d4cd959f7bb1d32738b74012ebf0
ahmed-alseidy <ahmedalseidy4@gmail.com>
2025-12-11 20:02:54 +0200

    refactor(auth): cleaner register ui

--- apps/web/app/(auth)/register/page.tsx
+++ apps/web/app/(auth)/register/page.tsx
@@ -1,18 +1,26 @@
 "use client";
 
+import { GalleryVerticalEnd } from "lucide-react";
 import Link from "next/link";
 import { RegisterForm } from "./register-form";
 
 export default function SignupPage() {
   return (
     <div className="flex w-full items-center justify-center">
-      <div className="hidden min-h-screen w-1/2 flex-col justify-between bg-primary p-12 text-primary-foreground md:flex">
-        <h3 className="font-bold text-2xl">Create an account</h3>
-        <p className="w-4/5 text-muted">Register to your account to continue</p>
+      <div className="hidden min-h-screen w-1/2 flex-col justify-between bg-primary/5 p-12 text-primary-foreground md:flex">
+        <div className="flex items-center gap-2">
+          <GalleryVerticalEnd className="size-6 text-primary" />
+          <h3 className="font-semibold text-primary text-xl">Acme Inc.</h3>
+        </div>
+        <p className="w-4/5 text-primary">
+          Register to your account to continue
+        </p>
       </div>
       <div className="flex w-10/12 justify-center md:w-1/2">
         <div className="w-80 space-y-2">
-          <h1 className="text-center font-bold text-2xl">Create an account</h1>
+          <h1 className="text-center font-bold text-2xl text-primary">
+            Create an account
+          </h1>
           <RegisterForm />
           <div className="mt-2 flex justify-between text-sm">
             <p>
