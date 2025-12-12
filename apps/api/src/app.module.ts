f253b8e599aed5e835b60a4841c9ff3544881b99
ahmed-alseidy <ahmedalseidy4@gmail.com>
2025-12-12 21:04:22 +0200

    feat(auth): integrate AuthModule and implement workspace listing functionality in WorkspacesController

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from "@nestjs/common";
+import { AuthModule } from "@thallesp/nestjs-better-auth";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { auth } from "./lib/auth";
 import { WorkspacesModule } from "./workspaces/workspaces.module";
 
 @Module({
-  imports: [WorkspacesModule],
+  imports: [
+    WorkspacesModule,
+    AuthModule.forRoot({
+      auth,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/api/src/workspaces/workspaces.controller.ts
+++ apps/api/src/workspaces/workspaces.controller.ts
@@ -1,10 +1,6 @@
-import { Body, Controller, Post, Req } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { ApiCookieAuth } from "@nestjs/swagger";
-import {
-  OptionalAuth,
-  Session,
-  type UserSession,
-} from "@thallesp/nestjs-better-auth";
+import { Session, type UserSession } from "@thallesp/nestjs-better-auth";
 import { CreateWorkspaceDto } from "./dto/create-workspace.dto";
 import { WorkspacesService } from "./workspaces.service";
 
@@ -14,15 +10,22 @@ export class WorkspacesController {
   constructor(private readonly workspacesService: WorkspacesService) {}
 
   @Post()
-  @OptionalAuth()
   async createWorkspace(
     @Body() body: CreateWorkspaceDto,
-    @Session() session: UserSession,
-    @Req() req
+    @Session() session: UserSession
   ) {
     return await this.workspacesService.createWorkspace(
       body.name,
       body.slug,
+      session.user.id
+    );
+  }
+
+  @Get()
+  async list(
+    @Session() session: UserSession,
+  ) {
+    return await this.workspacesService.getWorkspaces(
       session.user.id
     );
   }
