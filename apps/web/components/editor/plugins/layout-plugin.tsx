d508f63dec8fb87156ae7ddcce03dd707b323b64
Ahmed <ahmedelsharawy117@gmail.com>
2026-06-09 12:57:43 +0300

    refactor: trust LAYOUTS[0]

--- apps/web/components/editor/plugins/layout-plugin.tsx
+++ apps/web/components/editor/plugins/layout-plugin.tsx
@@ -62,7 +62,7 @@ export function InsertLayoutDialog({
   activeEditor: LexicalEditor;
   onClose: () => void;
 }): JSX.Element {
-  const [layout, setLayout] = useState(LAYOUTS[0].value);
+  const [layout, setLayout] = useState(LAYOUTS[0]!.value);
   const buttonLabel = LAYOUTS.find((item) => item.value === layout)?.label;
 
   const onClick = () => {
